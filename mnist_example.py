e15d599a95c0fd5b4091c7e81f2709bd64d0a413
vietnamican <vietnamican@gmail.com>
2020-12-05 21:09:51 +0700

    Using einops instead of flatten

--- mnist_example.py
+++ mnist_example.py
@@ -33,12 +33,11 @@ test_loader = torch.utils.data.DataLoader(
                              ])),
   batch_size=batch_size_test, shuffle=True)
 
+# Design the structure of model
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-
-# Design the structure of model
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -57,8 +56,30 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x)
 
+# Add new model using einops instead of flatten
+from einops import rearrange, reduce, asnumpy, parse_shape
+from einops.layers.torch import Rearrange, Reduce
+
+class Net2(nn.Module):
+    def __init__(self):
+        super(Net2, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = Rearrange('b c h w -> b (c h w)')(x)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
 # Variables during training and testing
-network = Net()
+network = Net2()
 optimizer = optim.SGD(network.parameters(), lr=learning_rate,
                       momentum=momentum)
 train_losses = []
